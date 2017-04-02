6244bbb0326a854a07b42520a4d307d3c32a5654
Nikita Udovenko <daylight@shelke.org>
2017-04-02 18:15:30 +0300

    Add readme and demo

---
+++ README.md
@@ -0,0 +1,11 @@
+# Paysera task
+
+To run demo
+
+    composer install --no-dev
+    php demo.php tests/input.csv
+
+To run tests
+
+    composer install
+    vendor/bin/phpunit tests
---
+++ demo.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+if (!isset($argv[1]) || !is_file($infile = $argv[1])) {
+    printf("php %s <infile>\n", __FILE__);
+    exit(1);
+}
+
+$transactionsQuery = new \Query\CalculateCommissionsFromTransactionsCSV(
+    $infile,
+    [
+        'EUR' => [
+            'rate' => 1.,
+            'precision' => 2,
+        ],
+        'USD' => [
+            'rate' => 1.1497,
+            'precision' => 2,
+        ],
+        'JPY' => [
+            'rate' => 129.53,
+            'precision' => 0,
+        ]
+    ]
+);
+
+try {
+    $transactions = $transactionsQuery->execute();
+    echo implode("\n", $transactions);
+} catch (Exception $ex) {
+    echo $ex->getMessage();
+    exit(1);
+}
